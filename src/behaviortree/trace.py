23fd2bd2b566f05047af1a4769155a50ccf7b3cc
0xFireball <0xFireball@outlook.com>
2018-08-13 13:20:46 -0700

    don't duplicate name in trace

--- src/behaviortree/trace.py
+++ src/behaviortree/trace.py
@@ -7,7 +7,10 @@ def dump_tree(tree: Composite, n: int = 1):
     symbol = ">"
     if is_composite: symbol = "-"
     if is_decorator: symbol = "@"
-    display = f"|{symbol} {tree._name}[{tree.__class__.__name__}]"
+    item_name = tree._name
+    item_type = tree.__class__.__name__
+    if item_name == item_type: item_type = ""
+    display = f"|{symbol} {item_name}[{item_type}]"
     print("  " * n + display)
     # dump children
     if is_composite:
--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='BehaviorTree',
-    version='0.1.3-dev',
+    version='0.1.4-dev',
     packages=['behaviortree',],
     license='Apache 2.0'
 )
